9a2e96bbe62f965452e5d0830bf8659b290d3d57
dicky08 <dickyf147@gmail.com>
2020-08-19 19:01:08 +0700

    editDivide.js

--- implement_Program/divideSort.js
+++ implement_Program/divideSort.js
@@ -1,19 +1,17 @@
 function divide(angka) {
-    if (typeof angka !== 'number') {
-        console.log('Masukan Number');
-    } else {
-        let arr = angka.toString()
-        let spl = arr.split("0");
-        // Gunakan Destructuring
-        let [arr1, arr2, arr3] = spl;
-        // Split Lalu Sorting Angka
-        let sor1 = arr1.split('').sort();
-        let sor2 = arr2.split('').sort();
-        let sor3 = arr3.split('').sort();
-        // Gabung dengan Spread Operator
-        let gabung = [...sor1, ...sor2, ...sor3];
-        console.log(Number(gabung.join('')));
-    }
-
+  console.log(angka);
+  if (typeof angka !== "number") {
+    console.log("Masukan Number");
+  } else {
+    let arr = angka.toString();
+    let spl = arr.split("0");
+    let kosong = [];
+    spl.map((m) => {
+      let sorting = m.split("").sort();
+      let ubahAngka = Number(sorting.join(""));
+      kosong += ubahAngka;
+    });
+    console.log(kosong);
+  }
 }
-divide(1232304340342);
+divide(1324509876504321);
