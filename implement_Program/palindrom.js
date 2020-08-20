d67a18891c582815d4c690cda86cc07c1ef02a42
dicky08 <dickyf147@gmail.com>
2020-08-20 09:41:15 +0700

    run

--- implement_Program/palindrom.js
+++ implement_Program/palindrom.js
@@ -1,18 +1,19 @@
 function palindrome(params) {
     let ubahStr = params.toString();
-    let panjang = ubahStr.length;
+    let kecilkan = ubahStr.toLowerCase();
+    let panjang = kecilkan.length;
     if (params == '') {
         console.log('Tidak Boleh Kosong');
         return false;
     }
     let str = '';
     for (let i = panjang - 1; i >= 0; i--) {
-        str += ubahStr[i];
+        str += kecilkan[i];
     }
-    if (ubahStr !== str) {
+    if (kecilkan !== str) {
         console.log('Bukan Palindrom');
     } else {
         console.log('Palindrom');
     }
 }
-palindrome('');
+palindrome('Malam');
